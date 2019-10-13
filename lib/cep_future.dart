158ef113873125259e5a07e262427f2b7e773952
Leonardo Cardoso <leocardosoti@gmail.com>
2019-10-13 12:57:23 -0300

    implementing future success

--- lib/cep_future.dart
+++ lib/cep_future.dart
@@ -1,5 +1,7 @@
 library cep_future;
 
+import 'dart:async';
+
 import 'package:cep_future/error.dart';
 import 'package:cep_future/models/cep.dart';
 import 'package:cep_future/services/correios.dart';
@@ -7,16 +9,41 @@ import 'package:cep_future/services/via_cep.dart';
 
 const CEP_SIZE = 8;
 
-Future<Cep> fetchCepFromServices(String cepWithLeftPad) => Future.any([
-      fetchViaCepService(cepWithLeftPad),
-      fetchCorreiosService(cepWithLeftPad),
-    ]);
+Future<Cep> fetchCepFromServices(String cepWithLeftPad) {
+  final completer = Completer<Cep>();
+
+  final errors = <Error>[];
+
+  final onValue = (Cep value) {
+    if (!completer.isCompleted) completer.complete(value);
+  };
+
+  final futures = [
+    fetchCorreiosService(cepWithLeftPad),
+    fetchViaCepService(cepWithLeftPad)
+  ];
+
+  final onError = (error, StackTrace stack) {
+    errors.add(error);
+
+    if (!completer.isCompleted && futures.length == errors.length) {
+      completer.completeError(error, stack);
+    }
+  };
+
+  for (var future in futures) {
+    future.then(onValue, onError: onError);
+  }
+
+  return completer.future;
+}
 
 void throwApplicationError(Object e) {
   throw e;
 }
 
 void handleServicesError(Object aggregatedErrors) {
+  print(aggregatedErrors.runtimeType);
   if (aggregatedErrors.runtimeType == ServiceError) {
     throw SimpleError('Todos os serviços de CEP retornaram erro.');
   }
